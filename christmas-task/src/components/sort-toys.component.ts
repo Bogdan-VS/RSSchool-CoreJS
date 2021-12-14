81b3da6b6987737710e3bf25289cc5b8ce410dd9
Bogdan Samonov <bogdansamonov@hmail.com>
2021-12-14 23:59:03 +0300

    feat: revision filter logic

--- christmas-task/src/components/sort-toys.component.ts
+++ christmas-task/src/components/sort-toys.component.ts
@@ -1,5 +1,6 @@
 export class SortToys {
-  constructor() {}
+  constructor() {
+  }
 
   sortToName(data: any[]) {
     return data.sort((a, b) => a.name.localeCompare(b.name));
@@ -11,5 +12,51 @@ export class SortToys {
 
   sortALL(data: any[]) {
     return data.sort((a, b) => (a.num - b.num));
+  }
+
+  getCurrentActiveElements() {
+    const dataActiveItem: string[] = [];
+
+    const massForm = document.querySelectorAll('.form-icon');
+    const countCopyes = document.querySelector('.count-copy-item');
+    const year = document.querySelector('.year-purchase-item');
+    const colorContainer = document.querySelectorAll('.color-item');
+    const sizeItem = document.querySelectorAll('.size-item');
+    const favoriteToys = document.querySelector('.favorite-item');
+
+    massForm.forEach(element => {
+      if (element.classList.contains('form-icon__active')) {
+        dataActiveItem.push(element.getAttribute('data-form'));
+      }
+    });
+
+    for (let i = 1; i <= +(countCopyes as any).value; i++) {
+      dataActiveItem.push(String(i));
+    }
+
+    for (let i = 1940; i <= (+(year as any).value + 1940); i++) {
+      dataActiveItem.push(String(i));
+    }
+
+    colorContainer.forEach(element => {
+      if (element.classList.contains('color-item__active')) {
+        dataActiveItem.push(element.getAttribute('data-color'));
+      }
+    });
+
+    sizeItem.forEach(element => {
+      if ((element as any).checked) {
+        dataActiveItem.push(element.getAttribute('data-size'));
+      }
+    });
+
+    if ((favoriteToys as any).checked) {
+      (dataActiveItem as any).push(true);
+    } else {
+      (dataActiveItem as any).push(true);
+      (dataActiveItem as any).push(false);
+    }
+
+    return dataActiveItem;
   }
 } 
--- christmas-task/src/components/toys.component.ts
+++ christmas-task/src/components/toys.component.ts
@@ -5,6 +5,7 @@ export class Toys extends App {
   newData: any;
   dataToys: any;
   sortToys: SortToys;
+  countCopy: any;
   constructor(id: string) {
     super(id);
     this.dataToys;
@@ -16,16 +17,21 @@ export class Toys extends App {
     this.getData();
     const start: HTMLElement = document.querySelector('.start');
     const chooseItem: HTMLElement = document.querySelector('.choose-item');
+    const countCopy: HTMLElement = document.querySelector('.count-copy-item');
+    const yearPurchase: HTMLElement = document.querySelector('.year-purchase-item');
 
+    countCopy.addEventListener('input', this.changeCountCopy.bind(this));
+    yearPurchase.addEventListener('input', this.chageYearPurchase.bind(this));
     start.addEventListener('click', buttonHandler.bind(this));
     chooseItem.addEventListener('click', this.addSortToys.bind(this));
     this.$el.addEventListener('click', this.getSortToys.bind(this));
     this.$el.addEventListener('click', this.getFilterForm.bind(this));
     this.$el.addEventListener('click', this.applyBtnSucsses.bind(this));
+    this.$el.addEventListener('click', this.addActiveColor.bind(this));
   }
 
   async getData() {
-    let url: string = ('https://raw.githubusercontent.com/Bogdan-VS/image-data/master/data.json');
+    const url: string = ('https://raw.githubusercontent.com/Bogdan-VS/image-data/master/data.json');
     const res: Response = await fetch(url);
     const data = await res.json();
     this.dataToys = data;
@@ -50,7 +56,7 @@ export class Toys extends App {
           <li>Форма игрушки: ${data[i].shape}</li>
           <li>Цвет игрушки: ${data[i].color}</li>
           <li>Размер игрушки: ${data[i].size}</li>
-          <li>Любимая: ${data[i].favorite}</li>
+          <li>Любимая: ${data[i].like}</li>
         </ul>
       `
       collection.append(content);
@@ -67,39 +73,60 @@ export class Toys extends App {
   getSortToys(event: Event) {
     const currentCategories = document.getElementById('current-categories');
     const target = (event.target as HTMLElement).closest('.sort-item');
-    if (target) {
-      if ((target as HTMLTemplateElement).dataset.sort === 'По году выпуска') {
+
+    switch(target && (target as HTMLTemplateElement).dataset.sort) {
+      case 'По году выпуска':
         currentCategories.textContent = 'По году выпуска';
         this.addSortToys();
-      }
-      if ((target as HTMLTemplateElement).dataset.sort === 'Все игрушки') {
+        break;
+      case 'Все игрушки':
         currentCategories.textContent = 'Все игрушки';
         this.addSortToys();
-      }
-      if ((target as HTMLTemplateElement).dataset.sort === 'По имени') {
+        break;
+      case 'По имени':
         currentCategories.textContent = 'По имени';
         this.addSortToys();
-      }
+        break;
     }
+
+    // if (target) {
+    //   if ((target as HTMLTemplateElement).dataset.sort === 'По году выпуска') {
+    //     currentCategories.textContent = 'По году выпуска';
+    //     this.addSortToys();
+    //   }
+    //   if ((target as HTMLTemplateElement).dataset.sort === 'Все игрушки') {
+    //     currentCategories.textContent = 'Все игрушки';
+    //     this.addSortToys();
+    //   }
+    //   if ((target as HTMLTemplateElement).dataset.sort === 'По имени') {
+    //     currentCategories.textContent = 'По имени';
+    //     this.addSortToys();
+    //   }
+    // }
   }
 
   applyBtnSucsses() {
     const target = (event.target as HTMLElement).closest('#apply-filter');
+    const allCategories = document.getElementById('all');
     if (target) {
-      const massForm = document.querySelectorAll('.form-icon');
-      let massFormAttribures: string[] = [];
 
-      massForm.forEach(element => {
-        if (element.classList.contains('form-icon__active')) {
-          massFormAttribures.push(element.getAttribute('data-form'));
-        }
-      });
+      const massFilter: string[] = this.sortToys.getCurrentActiveElements();
+      console.log(massFilter);
 
       let succsessFilterForm = this.dataToys.filter((el: any) => {
-        return massFormAttribures.includes(el.shape);
+        return massFilter.includes(el.shape) &&
+          massFilter.includes(el.count) &&
+          massFilter.includes(el.year) &&
+          massFilter.includes(el.color) &&
+          massFilter.includes(el.size) &&
+          massFilter.includes(el.favorite);
       })
 
-      this.getResult(succsessFilterForm);
+      if ((allCategories as any).checked) {
+        this.getResult(this.dataToys);
+      } else {
+        this.getResult(succsessFilterForm);
+      }
     }
   }
 
@@ -128,6 +155,10 @@ export class Toys extends App {
   }
 
   getResult(arg: any[]) {
+
+    if (arg.length === 0) {
+      alert('Я еще не успел сделать нормальное окошно. Но не смотря на это, нужных тебе игрушек нет. Попробуй изменить критерии поиска. Удачи:)')
+    }
     let sortData;
     const currentCategories = document.getElementById('current-categories');
     if (currentCategories.textContent === 'По году выпуска') {
@@ -142,6 +173,51 @@ export class Toys extends App {
 
     this.removeData();
     this.drawToys(sortData);
+  }
+
+  changeCountCopy() {
+    const countCopy = document.querySelector('.count-copy-item');
+    const maxCount = document.querySelector('.count-max');
+
+    const value = String((countCopy as any).value * 100 / (countCopy as any).max);
+    const currentValue = (countCopy as any).value;
+    maxCount.textContent = `${+currentValue + 1}`;
+    (countCopy as any).style.background = `linear-gradient(to right, #24c5db 0%, #24c5db ${value}%, rgb(196, 196, 196) ${value}%, rgb(196, 196, 196) 100%)`;
+    console.log(value);
+  }
+
+  chageYearPurchase() {
+    const yearPurchase: HTMLElement = document.querySelector('.year-purchase-item');
+    const maxYear = document.querySelector('.year-max');
+
+    const value = String((yearPurchase as any).value * 100 / (yearPurchase as any).max);
+    const currentValue = +(yearPurchase as any).value + 1940;
+    maxYear.textContent = `${+currentValue}`;
+    (yearPurchase as any).style.background = `linear-gradient(to right, #24c5db 0%, #24c5db ${value}%, rgb(196, 196, 196) ${value}%, rgb(196, 196, 196) 100%)`;
+    console.log(value);
+  }
+
+  addActiveColor() {
+    const target = (event.target as HTMLElement).closest('.color-item');
+
+    switch (target && (target as HTMLTemplateElement).dataset.color) {
+      case "белый":
+        target.classList.toggle('color-item__active');
+        break
+      case "желтый":
+        target.classList.toggle('color-item__active');
+        break
+      case "красный":
+        target.classList.toggle('color-item__active');
+        break
+      case "синий":
+        console.log('синий');
+        target.classList.toggle('color-item__active');
+        break
+      case "зеленый":
+        target.classList.toggle('color-item__active');
+        break
+    }
   }
 
   removeData() {
